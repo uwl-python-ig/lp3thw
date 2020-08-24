81c680682d963a6a626c0434ab6776a8434ac119
Benjamin Riesenberg <ries07@uw.edu>
2020-08-23 20:35:11 -0700

    bmr tiny start on ex 36

---
+++ ex36/bmr_ex36.py
@@ -0,0 +1,16 @@
+"""
+The Tome of Excellence
+"""
+
+from sys import exit
+
+def begin():
+    print("""
+After much reading and study, you have been gifted with the Tome of Excellence by the Council of Gifting of the Tome of Excellence.
+This is Excellent!
+What is your first step?
+    """)
+    # pick back up here
+    exit(0)
+
+begin()
