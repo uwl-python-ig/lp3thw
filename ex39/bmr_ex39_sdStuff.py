3b9f39525ce44b353ccbc055bfe9c455acac07d3
briesenberg07 <ries07@uw.edu>
2020-11-12 12:01:28 -0800

    BMR ex39 puzzling

--- ex39/bmr_ex39_sdStuff.py
+++ ex39/bmr_ex39_sdStuff.py
@@ -1,18 +1,17 @@
 rule = '-' * 20
 
-# Let's create the dictionary on the command line;
-    # I don't want to do states/cities so I'll do food/ingredient
-
-# For when I want to make the list as I go:
-"""
 fi = {}
+
+"""
+# For when I want to make the list as I go:
+    # Let's create the dictionary on the command line;
+    # I don't want to do states/cities so I'll do food/ingredient
 
 for n in range(0,5):
     food = input("Enter a food you like: ")
     ingredient = input("Enter an ingredient in that food: ")
     fi[food] = ingredient
 """
-
 # For when I don't:
 
 fi = {
@@ -30,20 +29,58 @@ for f, i in list(fi.items()):
 
 print(rule)
 
+"""
+# For retrieving values:
+
 test = input("Enter the name of a food, and I'll give you an ingredient in it: ")
 ingredient = fi.get(test, "Oh, dang, sorry. That food isn't on my list.")
 print(ingredient)
 
 print(rule)
+"""
+
+print("""OK, let's make another dictionary.
+This one will provide names in Japanese for the foods you listed earlier.""")
+
+fej = {}
+"""
+# For creating the dict in the CLI:
+
+for f, i in list(fi.items()):
+    print(f"Here's a food in English: {f}.")
+    jpn = input(f"Enter the Japanese word for {f}: ")
+    fej[f] = jpn
+"""
+# For my purposes trying to code below, I'll make it ahead of time:
+
+fej = {
+'sushi': '寿司',
+'nachos': 'ナチョス',
+'pizza': 'ピザ',
+'burgers': 'ハンバーガー',
+'sammies': 'サンドイッチ',
+'oatmeal': 'オーツミール',
+}
+
+for eng, jpn in list(fej.items()):
+    print(f"English: {eng}")
+    print(f"Japanese: {jpn}")
 
 """
-print("OK, let's make another dictionary.")
+# This does not work
+    # The following may be a place to start:
+    # https://www.programiz.com/python-programming/methods/dictionary/items
 
-sc = {}
+print("OK, now we can list foods in English and Japanese, and ingredients in English:")
 
-for i in range(0,5):
-    state = input("Enter the name of a state: ")
-    # ... pick up here.
-    # The goal is to create a dict with 'state': ['city', 'city2', 'city3']
-    # (as many cities as the user wants to enter)
+for f, i in list(fi.items()):
+    print(f"The food {f}, in Japanese, is", fej[fi[f]])
+
+# Traceback (most recent call last):
+#  File "bmr_ex39_sdStuff.py", line 53, in <module>
+#    print(f"The food {f}, in Japanese, is", fej[fi[f]])
+# KeyError: 'rice'
 """
+
+# I'd still like to try something with one key, multiple values
+    # 'food': ['ingred1', 'ingred2', 'ingred3']
