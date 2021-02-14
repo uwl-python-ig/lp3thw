bb9b0206696e29fe920af2487c28f5fcb53ed023
Benjamin Riesenberg <ries07@uw.edu>
2021-02-13 20:16:31 -0800

    @briesenberg07 start in using the author's skeleton; add some questions in comments

--- ex43/bmr_ex43.py
+++ ex43/bmr_ex43.py
@@ -5,13 +5,9 @@ class Scene(object):
     def enter(self):
         pass
 
-class Engine(object):
+# class Engine *was* here--does it need to be here??
 
-    def __init__(self, scene_map):
-        pass
-
-    def play(self):
-        pass
+# Child classes Death, CentralCorridor, LaserWeaponArmory, TheBridge, EscapePod
 
 class Death(Scene):
 
@@ -21,7 +17,10 @@ class Death(Scene):
 class CentralCorridor(Scene):
 
     def enter(self):
-        pass
+        # Test
+        print("You find yourself in the central corridor of this spaceship...")
+        # Ah... of course this string is not returned yet
+        # This is because class Engine has not been defined yet!
 
 class LaserWeaponArmory(Scene):
 
@@ -38,17 +37,42 @@ class EscapePod(Scene):
     def enter(self):
         pass
 
+class Engine(object):
+
+    def __init__(self, scene_map):
+        pass
+
+    def play(self):
+        pass
+
 class Map(object):
+    # Map questions:
+    # I don't understand why the Map class needs a start_scene attribute as well as an opening_scene method
+
+    # NOTE001
+    # ALRIGHT I'm going to cheat a bit and grab the "answer" for class Map as a way to get started
+
+    scenes = {
+        'central_corridor': CentralCorridor(),
+        'laser_weapon_armory': LaserWeaponArmory(),
+        'the_bridge': TheBridge(),
+        'escape_pod': EscapePod(),
+        'death': Death(),
+        # still need to define finished
+        # 'finished': Finished(),
+
+    }
 
     def __init__(self, start_scene):
-        pass
+        self.start_scene = start_scene
 
     def next_scene(self, scene_name):
-        pass
+        val = Map.scenes.get(scene_name)
+        return val
 
     def opening_scene(self):
-        pass
+        return self.next_scene(self.start_scene)
 
-a_map = Map('central corridor')
+a_map = Map('central_corridor')
 a_game = Engine(a_map)
 a_game.play()
